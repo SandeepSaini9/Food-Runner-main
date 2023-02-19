e695ca1c19ded73a231a3b7b379f4676ec4886b6
Sandeep <sandeepsaini74608@gmail.com>
2023-02-20 02:53:28 +0530

    aa

---
+++ app/src/main/java/com/sandeep/foodrunner/Activity/OtpActivity.kt
@@ -0,0 +1,12 @@
+package com.sandeep.foodrunner.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sandeep.foodrunner.R
+
+class OtpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+    }
+}
